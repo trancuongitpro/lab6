2f826bb0f5836218779a5c6e8615321c5b373f50
cuongtran <trancuongitpro@gmail.com>
2021-06-17 18:34:29 +0700

    Update lab6

---
+++ Lab6/Program.cs
@@ -0,0 +1,30 @@
+﻿using Lab6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thermostat thermostat = new Thermostat();
+            Heater heater = new Heater(60);
+            thermostat.OnTemperatureChange += heater.OnTemperatureChanged;
+            Heater heaterCuong = new Heater(100);
+            thermostat.OnTemperatureChange +=
+            heaterCuong.OnTemperatureChanged;
+            Cooler cooler = new Cooler(80);
+            thermostat.OnTemperatureChange +=
+            cooler.OnTemperatureChanged;
+            string temperature;
+            Console.Write("Enter temperature: ");
+            temperature = Console.ReadLine();
+            thermostat.CurrentTemperature = int.Parse(temperature);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        public delegate void Increment(ref int X);
+        public static void Add2(ref int x)
+        {
+            x += 2;
+        }
+        public static void Add3(ref int x)
+        {
+            x += 3;
+        }
+        public static void Main()
+        {
+            Increment functionDelegate = Add2;
+            functionDelegate += Add3;
+            functionDelegate += Add2;
+            int x = 5;
+            functionDelegate(ref x);
+            Console.ReadLine();
+        }
+    }
+}
